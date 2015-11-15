0f36d3f71808e111804226c6f460ab4ae56a53bb
Italo Miranda de Novais <italomirandadenovais@gmail.com>
2015-11-15 15:34:48 -0300

    Commit com os Externs

---
+++ cpp/include/Extern.h
@@ -0,0 +1,25 @@
+/*
+ * Extern.h
+ *
+ *  Created on: 15/10/2015
+ *      Author: Allexandre
+ */
+
+#ifndef EXTERN_H_
+#define EXTERN_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+int iniciar (char* porta);
+int ler();
+float getAltitude();
+boolean getQueda();
+int finalizar();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* EXTERN_H_ */
