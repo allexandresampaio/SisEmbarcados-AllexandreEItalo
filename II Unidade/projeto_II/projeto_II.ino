f8499b87d33a8139eb2986a0206ac4d7a48b0f0d
allexandresampaio <allexandresss@gmail.com>
2015-12-14 21:22:28 -0300

    revisão de código e atualização

--- II Unidade/projeto_II/projeto_II.ino
+++ II Unidade/projeto_II/projeto_II.ino
@@ -14,7 +14,6 @@ ADXL345 acel = ADXL345();
 #define P_TEMPERATURA 1
 #define P_PRESENCA 3
 
-
 #define RFID 1298
 
 //Define os valores que serao usados no deslocamento
@@ -59,13 +58,6 @@ void setup() {
   acel.setActivityThreshold(50);
   acel.setInactivityThreshold(50);
   acel.setTimeInactivity(10);
-
-  //acionamento da deteccao de atividade e inatividade
-  acel.setInterruptMapping(ADXL345_INT_ACTIVITY_BIT, ADXL345_INT1_PIN);
-  acel.setInterruptMapping(ADXL345_INT_INACTIVITY_BIT, ADXL345_INT1_PIN);
-
-  acel.setInterrupt(ADXL345_INT_ACTIVITY_BIT, 1);
-  acel.setInterrupt(ADXL345_INT_INACTIVITY_BIT, 1);
 }
 
 long lerSensoresRF(){
@@ -84,39 +76,31 @@ long lerSensoresRF(){
   long temperatura = analogRead(P_TEMPERATURA);
   //Mapeamento do potenciometro
   umidade = map(umidade, 0, 1023, 0, 100);
-  temperatura = map(temperatura, 0, 1023, 0, 20);
+  temperatura = map(temperatura, 0, 1023, -50, +50);
 
   long rf = RFID;
   
-  long info = rf << DESLOC_RFID;
-  info = info | (presenca << DESLOC_PRESENCA);
-  info = info | (vibracao << DESLOC_VIBRACAO);
-  info = info | (temperatura << DESLOC_TEMPERATURA);
+  long info = rf << DESLOC_RFID;//18
+  info = info | (presenca << DESLOC_PRESENCA);//17
+  info = info | (vibracao << DESLOC_VIBRACAO);///16
+  info = info | (temperatura << DESLOC_TEMPERATURA);//8
   info = info | umidade;
-
-
-//long id = RFID;
-//  long info = id << DESLOCAMENTO_RFID;
-//  info = info | (presenca << DESLOCAMENTO_PRESENCA);
-//  info = info | (vibraco << DESLOCAMENTO_VIBRACAO);
-//  info = info | (vibraco << DESLOCAMENTO_TEMPERATURA);
-//  info = info | umidade;
-//   return info;
 }
 
 void loop() {
   // emissao de dados
+  //Serial.println(lerSensoresRF());
   emitir(lerSensoresRF());
-
   //recepcao de dados
   long info = receber();
-  if(info == -1){
-    infoRF.presenca = extrairPresenca(info);
-    infoRF.vibracao = extrairVibracao(info);
+  
+  if(info != -1){
     infoRF.umidade = extrairUmidade(info);
     infoRF.temperatura = extrairTemperatura(info);
+    infoRF.vibracao = extrairVibracao(info);
+    infoRF.presenca = extrairPresenca(info);
     infoRF.id = extrairRFID(info);
-    enviarParaUSB();
+    //enviarParaUSB();
   }
   
   Serial.println("ID = "+infoRF.id);
@@ -126,11 +110,12 @@ void loop() {
   Serial.println("Vibracao = "+infoRF.vibracao);
   
   //Delay de 3 segundos para sincronizar com o sensor de presenca
-  //delay(3000);
+  delay(3000);
 }
 
 void emitir(long info){
   emissor.send(info, 32);
+  Serial.println(info);
 }
 
 long receber(){
@@ -145,29 +130,30 @@ long receber(){
 
 
 void enviarParaUSB(){
-//  char buff[sizeof(infoRF)]={0};
-//  memcpy(&buff, &infoRF, sizeof(infoRF));
-//  Serial.print('I');
-//  Serial.write(buff, sizeof(infoRF));
+  char buff[sizeof(infoRF)]={0};
+  memcpy(&buff, &infoRF, sizeof(infoRF));
+  Serial.write('I');
+  Serial.write(buff, sizeof(infoRF));
+  Serial.write('F');
 }
 
 int extrairRFID(long info){
-  int rf = (info & 66846720) >> DESLOC_RFID;
+  int rf = (info & 66846720) >> DESLOC_RFID;//18
   return rf;
 }
 
 int extrairPresenca(long info){
-  int presenca = (info & 131072) >> DESLOC_PRESENCA;
+  int presenca = (info & 131072) >> DESLOC_PRESENCA;//17
   return presenca;
 }
 
 int extrairVibracao(long info){
-  int vibracao = (info & 65536) >> DESLOC_VIBRACAO;
+  int vibracao = (info & 65536) >> DESLOC_VIBRACAO;//16
   return vibracao;
 }
 
 int extrairTemperatura(long info){
-  int temperatura = (info & 65280) >> DESLOC_TEMPERATURA;
+  int temperatura = (info & 65280) >> DESLOC_TEMPERATURA;//8
   return temperatura;
 }
 
