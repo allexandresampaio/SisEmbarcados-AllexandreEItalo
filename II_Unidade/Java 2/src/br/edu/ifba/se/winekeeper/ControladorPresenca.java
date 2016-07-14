bd487898802804b85f95fb8b2cea71e0a77ad350
allexandresampaio <allexandresss@gmail.com>
2016-07-14 10:14:50 -0300

    pasta Java

---
+++ II_Unidade/Java 2/src/br/edu/ifba/se/winekeeper/ControladorPresenca.java
@@ -0,0 +1,27 @@
+package br.edu.ifba.se.winekeeper;
+
+public class ControladorPresenca {
+	private static ControladorPresenca instance = null;
+	   protected ControladorPresenca() {
+	      // Exists only to defeat instantiation.
+	   }
+	   public static ControladorPresenca getInstance() {
+	      if(instance == null) {
+	         instance = new ControladorPresenca();
+	      }
+	      return instance;
+	   }
+	   
+	private static boolean presencaAnterior;
+	   
+	public static void setPresencaAnterior(boolean presenca) {
+			presencaAnterior = presenca;
+	}
+	
+	public static boolean isPresencaAnterior() {
+		return presencaAnterior;
+	}
+	   
+	   
+
+}
